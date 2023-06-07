68c0ca9d0e3b078a25f1d64a0d445a5249aa7231
Divine Quansah <divquan@gmail.com>
2023-06-07 17:58:05 +0000

    fixed: warning showing error messages

--- app/page.js
+++ app/page.js
@@ -81,16 +81,16 @@ export default function Home() {
 
 const InputField = ({ name, placeholder, value, onChange, type }) => {
   return (
-    <div class="flex flex-wrap -mx-3 mb-6">
-      <div class="w-full px-3">
+    <div className="flex flex-wrap -mx-3 mb-6">
+      <div className="w-full px-3">
         <label
-          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-          for=""
+          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          htmlFor={name}
         >
           {name}
         </label>
         <input
-          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           id="text"
           type={type}
           value={value}
@@ -98,7 +98,7 @@ const InputField = ({ name, placeholder, value, onChange, type }) => {
           placeholder={placeholder}
           accept={type === "file" ? "image/*" : null}
         />
-        {/* <p class="text-gray-600 text-xs italic">
+        {/* <p className="text-gray-600 text-xs italic">
           Make it as long and as crazy as you'd like
         </p> */}
       </div>
